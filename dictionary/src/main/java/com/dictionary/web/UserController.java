fa1563f3f3e18d0a99d3217c6ef97bdcf6840446
zeynepozdogan <zeynepozdogan36@gmail.com>
2021-06-02 15:30:28 +0300

    controllers are added

---
+++ dictionary/src/main/java/com/dictionary/web/UserController.java
@@ -0,0 +1,54 @@
+package com.dictionary.web;
+
+import com.dictionary.service.UserService;
+import com.dictionary.models.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.security.Principal;
+
+@Controller("/api/user")
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+public class UserController {
+
+    private final UserService userService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<User> findUsersOwnProfile(Principal principal) {
+        return new ResponseEntity<>(
+                userService.findUserByEmail(principal.getName()),
+                HttpStatus.OK
+        );
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        return new ResponseEntity<>(
+                userService.saveUser(user),
+                HttpStatus.OK
+        );
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public ResponseEntity<User> updateUser(Principal principal, @RequestBody User user) {
+        return new ResponseEntity<>(
+                userService.updateUser(principal.getName(), user),
+                HttpStatus.OK
+        );
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    public ResponseEntity<Void> deleteUser(Principal principal) {
+        userService.deleteUser(principal.getName());
+        return new ResponseEntity<>(
+                HttpStatus.NO_CONTENT
+        );
+    }
+
+}
---
+++ dictionary/src/main/java/com/dictionary/web/WordController.java
@@ -0,0 +1,53 @@
+package com.dictionary.web;
+
+import com.dictionary.service.WordService;
+import com.dictionary.models.Word;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Controller("/api/word")
+public class WordController {
+
+    private final WordService wordService;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Word> saveWord(@RequestBody Word word) {
+        return new ResponseEntity<>(
+                wordService.saveWord(word),
+                HttpStatus.OK
+        );
+    }
+
+    @RequestMapping(value = "/translate-turkish", method = RequestMethod.POST)
+    public ResponseEntity<Word> translateToTurkish(@RequestBody Word word) throws IOException {
+        return new ResponseEntity<>(
+                wordService.translateToTurkish(word),
+                HttpStatus.OK
+        );
+    }
+
+    @RequestMapping(value = "/translate-english", method = RequestMethod.POST)
+    public ResponseEntity<Word> translateToEnglish(@RequestBody Word word) throws IOException {
+        return new ResponseEntity<>(
+                wordService.translateToEnglish(word),
+                HttpStatus.OK
+        );
+    }
+
+    @RequestMapping(path = "/{wordId}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> deleteWord(@PathVariable String wordId) {
+        wordService.deleteWord(wordId);
+        return new ResponseEntity<>(
+                HttpStatus.NO_CONTENT
+        );
+    }
+}
