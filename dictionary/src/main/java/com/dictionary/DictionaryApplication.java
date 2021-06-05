99fe4a72255d4210cceb80175e4db2c55b0693aa
zeynepozdogan <zeynepozdogan36@gmail.com>
2021-06-05 21:47:14 +0300

    SecurityAutoConfiguration excluded from SpringBootApplication

--- dictionary/src/main/java/com/dictionary/DictionaryApplication.java
+++ dictionary/src/main/java/com/dictionary/DictionaryApplication.java
@@ -7,8 +7,9 @@ package com.dictionary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class DictionaryApplication {
 
     public static void main(String[] args) {
